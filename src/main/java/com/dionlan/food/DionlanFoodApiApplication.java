009d56aaf563e0663d09dabb608ff766a1df4dd9
dionlan <dionlan.alves@gmail.com>
2023-01-26 08:09:41 -0300

    First Commit!

---
+++ src/main/java/com/dionlan/food/DionlanFoodApiApplication.java
@@ -0,0 +1,14 @@
+package com.dionlan.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class DionlanFoodApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DionlanFoodApiApplication.class, args);
+    }
+
+}
