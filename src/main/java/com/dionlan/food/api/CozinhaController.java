70abd29f1f7fa03b9284f8916d7b933c41ff609a
dionlan <dionlan.alves@gmail.com>
2023-02-20 21:21:23 -0300

    Criando a camada de serviço para salvar cozinha.

--- src/main/java/com/dionlan/food/api/CozinhaController.java
+++ src/main/java/com/dionlan/food/api/CozinhaController.java
@@ -2,12 +2,15 @@ package com.dionlan.food.api;
 
 import com.dionlan.food.domain.model.Cozinha;
 import com.dionlan.food.domain.repository.CozinhaRepository;
+import com.dionlan.food.domain.service.CadastroCozinhaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-import java.util.Objects;
+import static java.util.Objects.nonNull;
+import static org.springframework.beans.BeanUtils.copyProperties;
 
 @RestController
 @RequestMapping("/cozinhas")
@@ -15,6 +18,9 @@ public class CozinhaController {
 
     @Autowired
     private CozinhaRepository cozinhaRepository;
+
+    @Autowired
+    private CadastroCozinhaService cadastroCozinhaService;
 
     @GetMapping
     public List<Cozinha> listar(){
@@ -25,7 +31,7 @@ public class CozinhaController {
     public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
         Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
 
-        if(Objects.nonNull(cozinha)){
+        if(nonNull(cozinha)){
             return ResponseEntity.ok(cozinha);
         }
 
@@ -34,6 +40,31 @@ public class CozinhaController {
 
     @PostMapping
     public void adicionar(@RequestBody  Cozinha cozinha){
-        cozinhaRepository.salvar(cozinha);
+        cadastroCozinhaService.salvar(cozinha);
+    }
+
+    @PutMapping("/{cozinhaId}")
+    public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha){
+        Cozinha cozinhaAtual = cozinhaRepository.buscar(cozinhaId);
+        if(nonNull(cozinhaAtual)){
+            copyProperties(cozinha, cozinhaAtual, "id");
+            cozinhaRepository.salvar(cozinhaAtual);
+            return ResponseEntity.ok(cozinhaAtual);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{cozinhaId}")
+    public ResponseEntity<Cozinha> remover(@PathVariable Long cozinhaId){
+        try{
+            Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+            if(nonNull(cozinha)){
+                cozinhaRepository.remover(cozinha);
+                return ResponseEntity.noContent().build();
+            }
+            return ResponseEntity.notFound().build();
+        }catch (DataIntegrityViolationException e){
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
     }
 }
---
+++ src/main/java/com/dionlan/food/domain/service/CadastroCozinhaService.java
@@ -0,0 +1,17 @@
+package com.dionlan.food.domain.service;
+
+import com.dionlan.food.domain.model.Cozinha;
+import com.dionlan.food.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroCozinhaService {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    public Cozinha salvar(Cozinha cozinha){
+        return cozinhaRepository.salvar(cozinha);
+    }
+}
