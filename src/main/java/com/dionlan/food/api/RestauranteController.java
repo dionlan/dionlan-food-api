cbd954d70f240662c6beb1eb46f7c5b65086698c
dionlan <dionlan.alves@gmail.com>
2023-03-18 15:11:34 -0300

    Criando endpoint que realiza o merge (PATCH);

--- src/main/java/com/dionlan/food/api/RestauranteController.java
+++ src/main/java/com/dionlan/food/api/RestauranteController.java
@@ -5,13 +5,18 @@ import com.dionlan.food.domain.model.Restaurante;
 import com.dionlan.food.domain.repository.CozinhaRepository;
 import com.dionlan.food.domain.repository.RestauranteRepository;
 import com.dionlan.food.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 
+import static java.util.Objects.isNull;
 import static java.util.Objects.nonNull;
 import static org.springframework.beans.BeanUtils.copyProperties;
 
@@ -68,5 +73,28 @@ public class RestauranteController {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @PatchMapping("/{restauranteId}")
+    public ResponseEntity<?> atualizarParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> camposOrigem){
+        Restaurante restauranteAtual = cadastroRestauranteService.buscar(restauranteId);
+        if(isNull(restauranteAtual)) {
+            ResponseEntity.notFound().build();
+        }
+        merge(camposOrigem, restauranteAtual);
+
+        return atualizar(restauranteId, restauranteAtual);
+    }
+
+    private void merge(Map<String, Object> camposOrigem, Restaurante restauranteDestino) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Restaurante restauranteOrigem = objectMapper.convertValue(camposOrigem, Restaurante.class);
+        camposOrigem.forEach((nomeProriedade, valorProriedade) -> {
+            Field field = ReflectionUtils.findField(Restaurante.class, nomeProriedade);
+            field.setAccessible(true);
+            Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+            System.out.println(nomeProriedade + " = " + valorProriedade + " = " + novoValor);
+            ReflectionUtils.setField(field, restauranteDestino, novoValor);
+        });
     }
 }
